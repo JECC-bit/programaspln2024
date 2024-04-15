8db435955a9979f636e93f5170691226508e604a
JECC-bit <jesusceballos2468@gmail.com>
2024-04-15 12:57:28 -0600

    leer texto de pagina web y obtener riqueza lexica

--- leerWEB.py
+++ leerWEB.py
@@ -11,25 +11,25 @@ def calcular_riqueza_lexica(tokens):
     return riqueza_lexica
 
 # URL de la página web que deseas analizar
-url = 'https://archiveos.org/pelican/'
+liga = input("Ingresa la URL de la página web a analizar: ")
 
 # Realizar la solicitud HTTP GET para obtener el contenido de la página
-response = requests.get(url)
+respuesta = requests.get(liga)
 
 # Verificar si la solicitud fue exitosa (código de estado 200)
-if response.status_code == 200:
+if respuesta.status_code == 200:
     # Parsear el contenido HTML utilizando BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(respuesta.content, 'html.parser')
     
     # Inicializar una lista para almacenar todos los tokens
     tokens_totales = []
     
     # En este punto, puedes acceder a elementos específicos de la página utilizando métodos de BeautifulSoup
     # Por ejemplo, para obtener todos los elementos de párrafo (<p>) en la página:
-    paragraphs = soup.find_all('p')
+    parrafos = soup.find_all('p')
     
     # Imprimir el texto de cada párrafo y calcular los tokens para cada uno
-    for paragraph in paragraphs:
+    for paragraph in parrafos:
         print(paragraph.text)
         tokens = nltk.word_tokenize(paragraph.text, language="english")
         tokens_totales.extend(tokens)  
@@ -38,4 +38,4 @@ if response.status_code == 200:
     riqueza_lexica_total = calcular_riqueza_lexica(tokens_totales)
     print("\n\n Riqueza léxica total: {:.2f}%".format(riqueza_lexica_total * 100))
 else:
-    print('Error al obtener la página:', response.status_code)
+    print('Error al obtener la página:', respuesta.status_code)
