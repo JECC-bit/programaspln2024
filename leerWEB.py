120978fe4cbb9cc841261d8baad7d658837589fe
JECC-bit <jesusceballos2468@gmail.com>
2024-04-15 12:45:32 -0600

    actividad de lectura de web

--- leerWEB.py
+++ leerWEB.py
@@ -2,6 +2,13 @@ import nltk
 import requests
 from bs4 import BeautifulSoup
 
+def calcular_riqueza_lexica(tokens):
+    tokens_conjunto = set(tokens)
+    palabras_totales = len(tokens)
+    palabras_diferentes = len(tokens_conjunto)
+    riqueza_lexica = palabras_diferentes / palabras_totales
+    return riqueza_lexica
+
 # URL de la página web que deseas analizar
 url = 'https://archiveos.org/pelican/'
 
@@ -13,15 +20,21 @@ if response.status_code == 200:
     # Parsear el contenido HTML utilizando BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
     
+    # Inicializar una lista para almacenar todos los tokens
+    tokens_totales = []
+    
     # En este punto, puedes acceder a elementos específicos de la página utilizando métodos de BeautifulSoup
     # Por ejemplo, para obtener todos los elementos de párrafo (<p>) en la página:
     paragraphs = soup.find_all('p')
     
-    # Imprimir el texto de cada párrafo
+    # Imprimir el texto de cada párrafo y calcular los tokens para cada uno
     for paragraph in paragraphs:
         print(paragraph.text)
-        tokens=nltk.word_tokenize(paragraph.text,"spanish")
-        riqueza_lexica=riqueza_lexica(tokens)
-    print("\n\n Riqueza léxica: ", riqueza_lexica*100,"%")
+        tokens = nltk.word_tokenize(paragraph.text, language="english")
+        tokens_totales.extend(tokens)  
+    
+    # Calcular la riqueza léxica utilizando la lista total de tokens
+    riqueza_lexica_total = calcular_riqueza_lexica(tokens_totales)
+    print("\n\n Riqueza léxica total: {:.2f}%".format(riqueza_lexica_total * 100))
 else:
     print('Error al obtener la página:', response.status_code)
